ece1996bf1516d09816fdb7c93cedf9d6bc01736
Justggodlike <sashanik212@gmail.com>
2020-04-06 17:43:24 +0300

    donations added

--- app/src/main/java/com/example/snake/GameOverActivity.java
+++ app/src/main/java/com/example/snake/GameOverActivity.java
@@ -22,6 +22,7 @@ public class GameOverActivity extends AppCompatActivity {
     ImageView restartButton;
     ImageView gameOverAnnouncement;
     ImageView finalScoreAnnouncement;
+    ImageView donateButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +44,7 @@ public class GameOverActivity extends AppCompatActivity {
             setContentView(R.layout.gameover_main);
             finalScoreReview = (TextView) findViewById(R.id.finalScoreReview);
             restartButton = (ImageView) findViewById(R.id.restartButton);
+            donateButton = (ImageView) findViewById(R.id.image_button_donate);
             gameOverAnnouncement = (ImageView) findViewById(R.id.gameOverAnnouncement);
             finalScoreAnnouncement = (ImageView) findViewById(R.id.finalScoreAnnouncement);
 
@@ -51,8 +53,10 @@ public class GameOverActivity extends AppCompatActivity {
             Bitmap bitmapFinalScoreAnnouncement = BitmapFactory.decodeResource(getResources(), R.drawable.text_final_score);
             Bitmap bitmapGameOverAnnouncement = BitmapFactory.decodeResource(getResources(), R.drawable.text_game_over);
             Bitmap bitmapRestartButton = BitmapFactory.decodeResource(getResources(), R.drawable.button_restart);
+            Bitmap bitmapDonateButton = BitmapFactory.decodeResource(getResources(), R.drawable.button_donate);
 
             restartButton.setImageBitmap(Bitmap.createScaledBitmap(bitmapRestartButton, (int) (screenWidth * 0.3625), (int) (screenWidth * 0.141), false));
+            donateButton.setImageBitmap(Bitmap.createScaledBitmap(bitmapDonateButton, (int) (screenWidth * 0.177), (int) (screenWidth * 0.2562), false));
             gameOverAnnouncement.setImageBitmap(Bitmap.createScaledBitmap(bitmapGameOverAnnouncement, (int) (screenWidth * 0.3583), (int) (screenWidth * 0.1393), false));
             finalScoreAnnouncement.setImageBitmap(Bitmap.createScaledBitmap(bitmapFinalScoreAnnouncement, (int) (screenWidth * 0.78125), (int) (screenWidth * 0.3038), false));
         }
@@ -62,6 +66,13 @@ public class GameOverActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SnakeActivity.gameOver = false;
                 startActivity(new Intent(GameOverActivity.this, SnakeActivity.class));
+            }
+        });
+
+        donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GameOverActivity.this, DonateActivity.class));
             }
         });
 
